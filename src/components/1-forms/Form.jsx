4fcf2a1e7b72eaf570e772f9d19994e926ed0f68
Koray KOKSAL <tkkoksal@icloud.com>
2023-07-06 20:51:26 +0300

    react form value

--- src/components/1-forms/Form.jsx
+++ src/components/1-forms/Form.jsx
@@ -1,5 +1,53 @@
+import { useState } from "react"
+
+
 const Form = () => {
-  return <div>Form</div>
+  const [username, setUsername] = useState("")
+
+  const hadleUsername=(e)=>{
+
+    setUsername(e.target.value)
+
+  }
+
+  return <div className="container mt-3">
+    
+    <h2 className="text-center text-success">FORM IN REACT</h2>
+    <form>
+
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1">Hello : {username}</label>
+        <input
+          type="text"
+          className="form-control"
+          id="username"
+          placeholder="Name"
+          value={username}
+          onChange={hadleUsername}
+        />
+
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          placeholder="Password"
+        />
+      </div>
+
+      <div className="text-center">
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </div>
+      
+    </form>
+
+
+  </div>
 }
 
 export default Form
